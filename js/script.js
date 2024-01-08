21b475956eb7533ad30c9efb21b058f291445dd4
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-08 14:41:51 +0100

    while print

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log('JS OK');
+
+//creo lista
+const listaSpesa = ['pane', 'salame', 'formaggio', 'pomodoro', 'lattuga', 'maionese', 'cocacola', 'pancetta', 'pasta', 'broccoli', 'cartaforno', 'peperoncino', 'olio', 'uova', 'latte', 'zucchine'];
+
+console.log(listaSpesa);
+
+//Prendo l'elemento del html dove scrivere
+const results = document.getElementById('lista');
+
+//Creo la lista
+let lista = '<ul>'
+
+//inizializzo variabile contatore
+let i = 0 
+
+// cilclo while
+while(listaSpesa.length > i){
+
+    let risultato = listaSpesa[i];
+    
+    lista += `<li>${risultato} </li>`
+    i++
+}
+console.log(i);
+
+lista += '</ul>'
